466e3cd634b58b12e28a447717893ae70c2b8f25
Filip <filip.eller@aalto.fi>
2022-06-30 18:47:53 +0300

    Complete 10a.

--- App.js
+++ App.js
@@ -2,6 +2,9 @@ import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
 
 export default function App() {
+  {
+    console.log('logging')
+  }
   return (
     <View style={styles.container}>
       <Text>Yeet</Text>
